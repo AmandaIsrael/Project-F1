6eed94c17642b3fe231f2371bf891719941f52a3
AmandaIsrael <amanda_israel@hotmail.com>
2021-08-23 18:41:08 -0300

    JavaFx with Scene Builder

---
+++ src/ProjectF1.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.*;
+
+public class ProjectF1 extends Application{
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Layout.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene screen = new Scene(root);
+
+        primaryStage.setTitle("F1");
+        primaryStage.setScene(screen);
+        primaryStage.show();
+    }
+}
